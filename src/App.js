20480f3ce716ddcf3e596f28f4b7fb2f14b63ea3
Fauzan Akmal Mahdi <recovery252@gmail.com>
2025-01-16 20:56:34 +0700

    page using react router dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Layout from './components/shared/Layout'
+import Dashboard from './components/Dashboard'
+import Products from './components/Products'
+
+
 function App() {
     return (
-        <div className="App">
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Dashboard />} />
+                    <Route path='products' element={<Products />} />
+                </Route>
+            </Routes>
+        </Router>
     )
 }
 
---
+++ src/components/Products.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Products(){
+    return <div>this is product page </div>
+}
+
+export default Products
