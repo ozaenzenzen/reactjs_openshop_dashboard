bb6f6cb48527b2a25a14e19155df31a9d6e8ed4e
Fauzan Akmal Mahdi <recovery252@gmail.com>
2025-01-17 09:41:15 +0700

    update dulu

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -3,8 +3,8 @@ import { Outlet } from 'react-router-dom'
 
 function Layout() {
     return (
-        <div>
-            <div className='bg-sky-100'>sidebar</div>
+        <div className='flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden'>
+            <div className='bg-sky-200'>sidebar</div>
             <div className='bg-teal-200'>header</div>
             <div>{<Outlet/>}</div>
             <p>footer</p>
