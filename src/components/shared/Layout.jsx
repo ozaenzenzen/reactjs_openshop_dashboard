71b0458ad795a2b9776d069dc6509bb3d320ec2e
Fauzan Akmal Mahdi <recovery252@gmail.com>
2025-01-19 12:24:36 +0700

    sidebar top bar

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Sidebar from './Sidebar'
 
 function Layout() {
     return (
-        <div className='flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden'>
-            <div className='bg-sky-200'>sidebar</div>
-            <div className='bg-teal-200'>header</div>
-            <div>{<Outlet/>}</div>
-            <p>footer</p>
+        <div className="flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden">
+            <Sidebar/>
+            <div className='p-4'>
+                <div className='bg-teal-200'>header</div>
+                <div>{<Outlet />}</div>
+            </div>
         </div>
     )
 }
 
-export default Layout 
+export default Layout
