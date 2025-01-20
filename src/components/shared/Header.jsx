3712d49fd19dcea0b8ee8d7f39caf9fd90770148
Fauzan Akmal Mahdi <recovery252@gmail.com>
2025-01-20 14:51:09 +0700

    implement popover

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import { HiOutlineSearch } from 'react-icons/hi'
+import { Popover, Transition } from '@headlessui/react'
+import classNames from 'classnames'
+import React, { Fragment } from 'react'
+import { HiOutlineBell, HiOutlineChatAlt, HiOutlineSearch } from 'react-icons/hi'
 
 function Header() {
     return (
@@ -12,7 +14,39 @@ function Header() {
                     className="text-sm focus:outline-none active:outline-none h-10 w-[24rem] border border-gray-300 rounded-sm pl-11 px-4"
                 />
             </div>
-            <div>side buttons</div>
+            <div className="flex items-center gap-2 mr-2">
+                <Popover className="relative">
+                    {({ open }) => (
+                        <>
+                            <Popover.Button
+                                className={classNames(
+                                    open && 'bg-gray-100',
+                                    'group inline-flex items-center rounded-sm p-1.5 text-gray-700 hover:text-opacity-100 focus:outline-none active:bg-gray-100'
+                                )}
+                            >
+                                <HiOutlineChatAlt fontSize={24} />
+                            </Popover.Button>
+                            <Transition
+                                as={Fragment}
+                                enter="transition duration-100 ease-out"
+                                enterFrom="transform scale-95 opacity-0"
+                                enterTo="transform scale-100 opacity-100"
+                                leave="transition duration-75 ease-out"
+                                leaveFrom="transform scale-100 opacity-100"
+                                leaveTo="transform scale-95 opacity-0"
+                            >
+                                <Popover.Panel className="absolute right-0 z-10 mt-2.5 w-80">
+                                    <div className="bg-white rounded-sm shadow-md ring-1 ring-black ring-opacity-5 px-2 py-2.5">
+                                        <strong className="text-gray-700 font-medium">Messages</strong>
+                                        <div className="mt-2 py-1 ">This is the panel</div>
+                                    </div>
+                                </Popover.Panel>
+                            </Transition>
+                        </>
+                    )}
+                </Popover>
+                <HiOutlineBell fontSize={24} />
+            </div>
         </div>
     )
 }
